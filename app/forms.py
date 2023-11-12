5fe4d20e350bc7f29e4d42c6d8a4cdbb55c96879
panini-19 <nianthony195@gmail.com>
2023-11-11 20:59:20 -0500

    day2flask

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+class pokeFormForm(FlaskForm):
+    name = TextAreaField('Enter Pokemon Name: ', validators=[DataRequired()])
+    submitButton = SubmitField('Search')
